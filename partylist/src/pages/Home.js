c4e88919063d62de722b0c167ef9c19c163633e1
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-04-29 22:00:13 -0400

    homepage css

--- partylist/src/pages/Home.js
+++ partylist/src/pages/Home.js
@@ -8,10 +8,14 @@ const Home = () => {
             <h1>Categories</h1>
         </div>
         <div className="categoriesContainer">
-            <div className="categories">Bakery</div>
+            <div className="categories">Cakes</div>
             <div className="categories">Balloon decorations</div>
-            <div className="categories">Party Characters</div>
+            <div className="categories">Party Entertainers</div>
             <div className="categories">Entertainment</div>
+            <div className="categories">Bounce houses</div>
+            <div className="categories">Event agencies</div>
+            <div className="categories">Photo and video services</div>
+            <div className="categories">DJ's</div>
         </div>  
         <div className="footer">
             <p>© 2021 by Anel Tuyebayeva. All rights reserved</p>
